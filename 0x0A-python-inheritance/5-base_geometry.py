3c77e3357d7a01c6180d9fc592f0d2d58d84665a
Dimpho Aphiri <dimplesaphiri@gmail.com>
2022-09-27 18:34:03 +0000

    inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Aphiri Dimpho <dimplesaphiri@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
